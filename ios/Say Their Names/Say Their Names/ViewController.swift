fdc81707c9d4c36e21cb8336ae0e0f3de82766dc
FranckNdame <franck.mpouli@yahoo.com>
2020-05-29 23:54:53 +0100

    add android version

---
+++ ios/Say Their Names/Say Their Names/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Say Their Names
+//
+//  Created by Franck-Stephane Ndame Mpouli on 29/05/2020.
+//  Copyright © 2020 Franck-Stephane Ndame Mpouli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
