33361d07b1978b7fca3e5030d5e7e1f1d4bedf11
Marcus Florentin <fmarcus28@gmx.fr>
2020-05-31 01:22:54 +0200

    Upgrading colors
    - Improving old colors to assets ones
    - Adding tint color

--- ios/Say Their Names/Say Their Names/Utils/Color/UIColor+Resource.swift
+++ ios/Say Their Names/Say Their Names/Utils/Color/UIColor+Resource.swift
@@ -12,9 +12,15 @@ extension UIColor {
     
     /// A collection of common colors
     enum STN {
-        static let black: UIColor = { UIColor.hexColor(0x101010) }()
-        static let white: UIColor = { UIColor.hexColor(0xffffff) }()
-        static let gray: UIColor = { UIColor.hexColor(0x9C9C9C) }()
+
+		static let black: UIColor = UIColor(named: "black") ?? #colorLiteral(red: 0.06274509804, green: 0.06274509804, blue: 0.06274509804, alpha: 1)
+		static let white: UIColor = UIColor(named: "white") ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+		static let gray: UIColor = UIColor(named: "grey") ?? #colorLiteral(red: 0.6117647059, green: 0.6117647059, blue: 0.6117647059, alpha: 1)
+
+
+		// MARK: - Applications
+
+		static let tint : UIColor = UIColor(named: "tint") ?? #colorLiteral(red: 0.06274509804, green: 0.06274509804, blue: 0.06274509804, alpha: 1)
 
 
 		// MARK: - Labels
