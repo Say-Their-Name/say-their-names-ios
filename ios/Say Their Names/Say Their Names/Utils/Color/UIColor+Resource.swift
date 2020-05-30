5484aad33bccd7807a0b7243ed419d4393172cc7
Marcus Florentin <fmarcus28@gmx.fr>
2020-05-31 00:49:56 +0200

    Ehancing color support
    Use of asset colors for `Interface Builder` + Dark mode
    - Add colors for label purposes

--- ios/Say Their Names/Say Their Names/Utils/Color/UIColor+Resource.swift
+++ ios/Say Their Names/Say Their Names/Utils/Color/UIColor+Resource.swift
@@ -15,5 +15,13 @@ extension UIColor {
         static let black: UIColor = { UIColor.hexColor(0x101010) }()
         static let white: UIColor = { UIColor.hexColor(0xffffff) }()
         static let gray: UIColor = { UIColor.hexColor(0x9C9C9C) }()
+
+
+		// MARK: - Labels
+
+		static let primaryLabel : UIColor = UIColor(named: "primaryLabel") ?? #colorLiteral(red: 0.06274509804, green: 0.06274509804, blue: 0.06274509804, alpha: 1)
+
+		static let secondaryLabel : UIColor = UIColor(named: "secondaryLabel") ?? #colorLiteral(red: 0.06274509804, green: 0.06274509804, blue: 0.06274509804, alpha: 0.47)
     }
+
 }
