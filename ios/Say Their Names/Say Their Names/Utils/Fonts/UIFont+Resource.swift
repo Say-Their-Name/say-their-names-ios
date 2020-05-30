0d08d76c479e8a4b9ccb5c4cedf83e7669fe7628
Marina Gornostaeva <gornostaevamv@gmail.com>
2020-05-30 23:53:57 +0200

    Fonts from Figma

--- ios/Say Their Names/Say Their Names/Utils/Fonts/UIFont+Resource.swift
+++ ios/Say Their Names/Say Their Names/Utils/Fonts/UIFont+Resource.swift
@@ -17,6 +17,22 @@ extension UIFont {
     
     /// A collection of common fonts
     enum STN {
-        static let titleFont: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaBold.rawValue, textStyle: TextStyle.headline)
+        static let title: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaBold.rawValue, textStyle: .title1) // 28
+        static let body: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaRegular.rawValue, textStyle: .body) // 18 on figma but this yields 17 on default
+        static let sectionHeader: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaBold.rawValue, textStyle: .body) // 17
+
+        static let filledButtonTitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaRegular.rawValue, textStyle: .body) // 17
+        static let plainButtonTitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaRegular.rawValue, textStyle: .title3) // 21 on figma but this yields 20 on default
+
+        static let cardTitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaBold.rawValue, textStyle: .subheadline) // 15
+        static let cardSubitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaRegular.rawValue, textStyle: .footnote) // 14 on figma but this yields 13 on default
+        
+        static let bannerTitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaBold.rawValue, textStyle: .title3) // 19 on figma but this yields 20 on default
+        static let bannerSubitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaRegular.rawValue, textStyle: .body) // 17
+
+        static let navBarTitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaRegular.rawValue, textStyle: .title2) // 21 on figma but this yields 22 on default
+        
+        static let tabButtonTitle: UIFont = UIFont.dynamicCustomFont(fontName: FontName.karlaBold.rawValue, textStyle: .footnote) // 13
+
     }
 }
