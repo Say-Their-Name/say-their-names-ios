7f6a1f4fdb9c721344b0cc78b232b529b3ef338b
Hakeem King <kingsqd1@gmail.com>
2020-06-02 10:42:52 -0700

    remove commenting out

--- Say Their NamesTests/Say_Their_NamesTests.swift
+++ Say Their NamesTests/Say_Their_NamesTests.swift
@@ -11,13 +11,13 @@ import XCTest
 
 class Say_Their_NamesTests: XCTestCase {
 
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
 
     func testExample() throws {
         // This is an example of a functional test case.
