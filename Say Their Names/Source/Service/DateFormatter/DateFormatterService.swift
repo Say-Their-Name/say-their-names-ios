beda38ad5e981c5e5f0e3f381c8efee22c9ab7c9
Hakeem King <kingsqd1@gmail.com>
2020-06-03 19:15:55 -0700

    create date formatter service

---
+++ Say Their Names/Source/Service/DateFormatter/DateFormatterService.swift
@@ -0,0 +1,56 @@
+//
+//  STNDateFormatter.swift
+//  Say Their Names
+//
+//  Created by Hakeem King on 6/3/20.
+//  Copyright © 2020 Franck-Stephane Ndame Mpouli. All rights reserved.
+//
+
+import Foundation
+
+protocol DateFormatterType {
+    func string(from date: Date) -> String
+}
+
+extension DateFormatter: DateFormatterType { }
+
+class DateFormatterService {
+
+    // MARK: - Shared
+
+    static let shared = DateFormatterService()
+    
+    // MARK: - Queue
+    
+    let dateFormattersQueue = DispatchQueue(label: "com.stn.date.formatter.queue")
+
+    // MARK: - Cached Formatters
+
+    private var dateFormatters = [String : DateFormatterType]()
+
+    private func cachedDateFormatter(withFormat format: String) -> DateFormatterType {
+        return dateFormattersQueue.sync {
+            let key = format
+            if let cachedFormatter = dateFormatters[key] {
+                return cachedFormatter
+            }
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = format
+            
+            dateFormatters[key] = dateFormatter
+            
+            return dateFormatters[key]!
+        }
+    }
+
+    // MARK: - Date Of Birth
+
+    func formatDOBDate(_ date: Date) -> String {
+        let dateFormatter = cachedDateFormatter(withFormat: "y/MM/dd @ HH:mm")
+        let formattedDate = dateFormatter.string(from: date)
+        return ("Date of birth: \(formattedDate)")
+    }
+
+}
---
+++ Say Their NamesTests/DateFormatterServiceTest/DateFormatterServiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  DateFormatterServiceTests.swift
+//  Say Their NamesTests
+//
+//  Created by Hakeem King on 6/3/20.
+//  Copyright © 2020 Franck-Stephane Ndame Mpouli. All rights reserved.
+//
+
+import XCTest
+@testable import Say_Their_Names
+
+class DateFormatterServiceTests: XCTestCase {
+    
+    func testLaunchScreenCanLoadNib() {
+        let dob = Date.dateFrom(year: 2020, month: 06, day: 03, hour: 4, minute: 11)!
+
+        let formattedDate = DateFormatterService.shared.formatDOBDate(dob)
+
+        XCTAssertEqual(formattedDate, "Date of birth: 2020/06/03 @ 04:11")
+    }
+}
+
+extension Date {
+    static func dateFrom(year: Int, month: Int,  day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) -> Date? {
+        var components = DateComponents()
+        components.year = year
+        components.month = month
+        components.day = day
+        components.hour = hour
+        components.minute = minute
+        components.second = second
+        components.timeZone = Calendar.current.timeZone
+
+        return Calendar.current.date(from: components)
+    }
+}
