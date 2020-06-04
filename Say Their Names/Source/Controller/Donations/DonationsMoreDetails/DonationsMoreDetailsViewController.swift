3d85cb525e63f1b594af3a94690aa22af240d0eb
Leonard Chan <wxclx98@gmail.com>
2020-06-04 13:10:34 +0800

    Create DonationsMoreDetailsViewController.

---
+++ Say Their Names/Source/Controller/Donations/DonationsMoreDetails/DonationsMoreDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DonationsMoreDetailsViewController.swift
+//  Say Their Names
+//
+//  Created by Leonard Chen on 6/4/20.
+//  Copyright © 2020 Franck-Stephane Ndame Mpouli. All rights reserved.
+//
+
+import UIKit
+
+final class DonationsMoreDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
