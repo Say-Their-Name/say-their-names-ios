41b61a84ba2d70230e6c75be3dd3edd976745698
ScruffyHipster <tomsmiled@me.com>
2020-06-04 08:29:06 +0100

    removed commented out code

--- Say Their Names/Source/Controller/Home/CollectionViewDataSource/PersonCollectionViewDataSource.swift
+++ Say Their Names/Source/Controller/Home/CollectionViewDataSource/PersonCollectionViewDataSource.swift
@@ -44,7 +44,6 @@ extension PersonCollectionViewDataSource: UICollectionViewDataSource {
     
   
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-//        if collectionView.tag == 0 { return UICollectionReusableView() }
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: CarouselHeaderView.reuseIdentifier, for: indexPath) as! CarouselHeaderView
            carouselDataResultsHandler = ResultsDataHandler(resultsData: self.carouselData)
            headerView.resultsHandler = carouselDataResultsHandler
