21013ccb5390c6bcdbaab08a5dfc58c84af56624
ScruffyHipster <tomsmiled@me.com>
2020-06-04 08:20:14 +0100

    Moved extension into correct file and removed unrequited file.

--- Say Their Names/Source/Utils/Extentions/UICollectionView++.swift
+++ Say Their Names/Source/Utils/Extentions/UICollectionView++.swift
@@ -24,3 +24,25 @@ extension UICollectionView {
         return cell
     }
 }
+
+extension UICollectionView {
+    func scrollToNearestVisibleCollectionViewCell() {
+        self.decelerationRate = UIScrollView.DecelerationRate.fast
+        let visibleCenterPositionOfScrollView = Float(self.contentOffset.x + (self.bounds.size.width / 2))
+        var closestCellIndex = -1
+        var closestDistance: Float = .greatestFiniteMagnitude
+        for cell in visibleCells {
+            let cellWidth = cell.bounds.size.width
+            let cellCenter = Float(cell.frame.origin.x + cellWidth / 2)
+            // Now calculate closest cell
+            let distance: Float = fabsf(visibleCenterPositionOfScrollView - cellCenter)
+            if distance < closestDistance {
+                closestDistance = distance
+                closestCellIndex = self.indexPath(for: cell)!.row
+            }
+        }
+        if closestCellIndex != -1 {
+            self.scrollToItem(at: IndexPath(row: closestCellIndex, section: 0), at: .centeredHorizontally, animated: true)
+        }
+    }
+}
