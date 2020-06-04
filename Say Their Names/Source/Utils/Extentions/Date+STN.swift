45f056ff300e485d380e20651cfda912bcd31340
Hakeem King <kingsqd1@gmail.com>
2020-06-04 02:29:34 -0700

    remove accidentally added file

--- Say Their Names/Source/Utils/Extentions/Date+STN.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Date+STN.swift
-//  Say Their Names
-//
-//  Created by Hakeem King on 6/4/20.
-//  Copyright © 2020 Franck-Stephane Ndame Mpouli. All rights reserved.
-//
-
-import Foundation
