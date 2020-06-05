bd75085c244a27863b09f4fd8e84b298687aab7e
Pedro Trujillo V <ptrujillo@crunchy.co>
2020-06-04 22:51:06 -0400

    Added Spanish localization for: PersonHeaderCell, PersonController, also added Localization for new static text for English, Spanish and Rusian

--- SayTheirNames/Source/Controller/Home/Person/PersonController.swift
+++ SayTheirNames/Source/Controller/Home/Person/PersonController.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 class PersonController: BaseViewController {
-
+    @IBOutlet weak var readMoreButton: UIButton!
+    @IBOutlet weak var donateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.accessibilityIdentifier = "personView"
 
         // Do any additional setup after loading the view.
+        setupLocalizedStrings()
     }
+    
+    private func setupLocalizedStrings() {
+        if let readMore = NSLocalizedString("Read_More", comment: "Read More") as String? {
+            self.readMoreButton.setTitle(readMore, for: UIControl.State.normal)
+            self.readMoreButton.setTitle(readMore, for: UIControl.State.selected)
+        } else {
+            self.readMoreButton.setTitle("READ MORE ->", for: UIControl.State.normal)
+            self.readMoreButton.setTitle("READ MORE ->", for: UIControl.State.selected)
+        }
+        
+        if let donate = NSLocalizedString("Donate", comment: "Donate") as String? {
+            self.donateButton.setTitle(donate, for: UIControl.State.normal)
+            self.donateButton.setTitle(donate, for: UIControl.State.selected)
+        } else {
+            self.donateButton.setTitle("Donate", for: UIControl.State.normal)
+            self.donateButton.setTitle("Donate", for: UIControl.State.selected)
+        }
+    }
+    
     
     @IBAction func didPressCloseButton() {
         self.dismiss(animated: true, completion: nil)
--- SayTheirNames/Source/View/Home/Person/PersonHeaderCell.swift
+++ SayTheirNames/Source/View/Home/Person/PersonHeaderCell.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class PersonHeaderCell: UICollectionViewCell {
     static let headerIdentifier = "PersonHeaderCell"
+    @IBOutlet weak var howToGetInvolvedLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.setupLocalizedStrings()
+    }
+    
+    private func setupLocalizedStrings() {
+        if let howToGetInvolved = NSLocalizedString("How_to_get_involved", comment: "How To Get Involved") as String? {
+            self.howToGetInvolvedLabel.text = howToGetInvolved
+        } else {
+            self.howToGetInvolvedLabel.text = "How to get involved"
+        }
     }
 
 }
