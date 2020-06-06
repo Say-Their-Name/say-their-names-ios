592222966fac380cdd32384b4681f9c3ed211c7e
EvilPenguin <evilpenguin@evilpengu.in>
2020-06-05 19:49:39 -0700

    git add city search to person;

--- SayTheirNames/Source/Service/Networking/Requestors/PeopleNetworkRequestor.swift
+++ SayTheirNames/Source/Service/Networking/Requestors/PeopleNetworkRequestor.swift
@@ -30,6 +30,7 @@ enum PersonEnvironment {
     static let baseUrlSring: String = { return "\(Environment.serverURLString)/api/people" }()
     static let peopleSearchString: String = { return "\(PersonEnvironment.baseUrlSring)/?name=" }()
     static let countrySeachUrlString: String = { return "\(PersonEnvironment.baseUrlSring)/?country=" }()
+    static let citySeachUrlString: String = { return "\(PersonEnvironment.baseUrlSring)/?city=" }()
 }
 
 // MARK: - NetworkRequestor (People)
@@ -57,6 +58,11 @@ extension NetworkRequestor {
     
     public func fetchPeopleByCountry(_ country: String, completion: @escaping (Result<PersonsResponsePage, AFError>) -> Swift.Void) {
         let url = "\(PersonEnvironment.countrySeachUrlString)\(country)"
+        self.fetchDecodable(url, completion: completion)
+    }
+    
+    public func fetchPeopleByCity(_ city: String, completion: @escaping (Result<PersonsResponsePage, AFError>) -> Swift.Void) {
+        let url = "\(PersonEnvironment.citySeachUrlString)\(city)"
         self.fetchDecodable(url, completion: completion)
     }
 }
