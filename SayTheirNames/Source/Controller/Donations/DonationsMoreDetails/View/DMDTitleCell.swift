eaf5a159eec8598bc4a779216bd137e984b047d0
Leonard Chan <wxclx98@gmail.com>
2020-06-07 16:54:59 +0800

    [FIX] Bring donation files back.

---
+++ SayTheirNames/Source/Controller/Donations/DonationsMoreDetails/View/DMDTitleCell.swift
@@ -0,0 +1,46 @@
+//
+//  DMDTitleCell.swift
+//  SayTheirNames
+//
+//  Created by Leonard Chen on 6/5/20.
+//  Copyright © 2020 Franck-Stephane Ndame Mpouli. All rights reserved.
+//
+
+import UIKit
+
+final class DMDTitleCell: UICollectionViewCell {
+    // MARK: - Property
+    static let reuseIdentifier = "donations-more-details-title-cell"
+    
+    // MARK: - View
+    private let titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.STN.title
+        return label
+    }()
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureSubview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Class Method
+    private func configureSubview() {
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.fillSuperview(superView: contentView, padding: .zero)
+    }
+    
+    // MARK: - Configure Cell
+    func configure(for donation: Donation) {
+        titleLabel.text = donation.title
+    }
+}
