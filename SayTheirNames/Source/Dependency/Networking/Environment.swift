cb2144806b09223cde31ddc428c49795b8701323
Marina <gornostaevamv@gmail.com>
2020-06-12 22:59:08 +0200

    [FIX] Disable ko & pt-BR locales (#215)
    * Disabled korean and brazillian/portugese translations
    
    * Fix base url removing those that don't exist

--- SayTheirNames/Source/Dependency/Networking/Environment.swift
+++ SayTheirNames/Source/Dependency/Networking/Environment.swift
@@ -25,14 +25,6 @@ import Foundation
 
 enum Environment {
     static let serverURLString: String = {
-        #if DEV
-            return "https://saytheirnames.dev"
-        #elseif QA
-            return "https://saytheirnames.qa"
-        #elseif LOCAL
-            return "https://localhost"
-        #else
-            return "https://saytheirnames.dev" // switch to PROD
-        #endif
+        "https://saytheirnames.dev"
     }()
 }
