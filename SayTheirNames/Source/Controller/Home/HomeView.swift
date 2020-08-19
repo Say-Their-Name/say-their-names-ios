87d8f8c083523b49b648138fad118f5cb8a6f2b7
Marina <gornostaevamv@gmail.com>
2020-08-19 13:42:01 +0200

    Making sure names fit on home for most text sizes (#251)

--- SayTheirNames/Source/Controller/Home/HomeView.swift
+++ SayTheirNames/Source/Controller/Home/HomeView.swift
@@ -84,16 +84,33 @@ final class HomeView: UIView {
             
             case .main:
                 
+                let sizeCategory = layoutEnviroment.traitCollection.preferredContentSizeCategory
+                let heightMultiplier: CGFloat
+                if sizeCategory >= .accessibilityExtraLarge {
+                    heightMultiplier = 1.5
+                }
+                else if sizeCategory >= .accessibilityMedium {
+                    heightMultiplier = 1.3
+                }
+                else if sizeCategory >= .extraExtraLarge {
+                    heightMultiplier = 1.15
+                }
+                else {
+                    heightMultiplier = 1.0
+                }
+                
+                let height: CGFloat = Theme.Screens.Home.CellSize.peopleHeight * CGFloat(heightMultiplier)
+
                 let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
                 let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
 
-                let height = Theme.Screens.Home.CellSize.peopleHeight
                 let columns = deviceWidth == .compact ? Theme.Screens.Home.Columns.compactScreenWidth : Theme.Screens.Home.Columns.wideScreenWidth
                 let layoutGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(height))
                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: layoutGroupSize, subitem: layoutItem, count: columns)
                 group.interItemSpacing = .fixed(Self.horizontalPaddingBetween)
                 
                 let mainSection = NSCollectionLayoutSection(group: group)
+                mainSection.interGroupSpacing = Self.horizontalPaddingBetween * 2
                 section = mainSection
                 
             case .header:
